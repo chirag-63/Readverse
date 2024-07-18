ea9271037e7102181432e8934551c30d9b579690
chirag <chiragchauhanx@gmail.com>
2024-07-19 00:29:31 +0530

    fixed minor bug

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,5 +1,5 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import appLogo from '../assets/logo/readverse-logo.png'
 import { Link } from 'react-router-dom';
 
