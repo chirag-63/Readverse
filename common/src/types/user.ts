3a54f0438b32e2901ae94fb69d10e57939798c1d
chirag <chiragchauhanx@gmail.com>
2024-07-19 00:26:15 +0530

    commit minor changes

--- common/src/types/user.ts
+++ common/src/types/user.ts
@@ -3,12 +3,12 @@ import z from 'zod'
 export const signupSchema = z.object({
     name: z.string(),
     email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().min(6, {message: "password must be 6 or more characters long"})
+    password: z.string().min(6, {message: "password must be 6 to 12 characters long"})
 })
 
 export const signinSchema = z.object({
     email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().min(6, {message: "password must be 6 or more characters long"})
+    password: z.string().min(6, {message: "password must be 6 to 12 characters long"})
 })
 
 //type inference
